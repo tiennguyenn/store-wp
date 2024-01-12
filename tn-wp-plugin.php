70dfd4e525888b49b40fec58959d75653779fff1
Tien Nguyen <tienn6981@gmail.com>
2024-01-12 11:35:14 +0700

    initial wp plugin

---
+++ tn-wp-plugin.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Plugin Name: TN WP Plugin
+ * Description: This is a wp plugin
+ * Author: TN
+ * Version: 1.0.18
+ *
+ * Requires at least: 5.0
+ * WC requires at least: 5.0
+ * WC tested up to: 8.0
+ */
+
+defined( 'ABSPATH' ) or exit;
